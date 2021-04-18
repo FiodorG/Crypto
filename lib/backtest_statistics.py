8de5bb91091ad463bb29de0521f7d05b471d779d
Fedor Gorokhovik <fedor.gorokhovik@gmail.com>
2021-04-18 14:00:54 +0100

    Add correct weight and rescale to $100 in momentum strategy

--- lib/backtest_statistics.py
+++ lib/backtest_statistics.py
@@ -1,14 +1,25 @@
 import pandas as pd
 import numpy as np
 
-
-def sharpe(df: pd.DataFrame) -> float:
-    mean = df.mean()
-    std = df.std()
-    return np.sqrt(252) * mean / std
+from .constants import DAYS_IN_YEAR
 
 
-def rolling_sharpe(df: pd.DataFrame, window: int) -> float:
-    mean = df.rolling(window).mean()
-    std = df.rolling(window).std()
-    return np.sqrt(252) * mean / std
+def account_curve_statistics(acc_curve: pd.DataFrame):
+    stats = {
+        "Mean $Return": acc_curve.diff().mean() * DAYS_IN_YEAR,
+        "Stddev $Return": acc_curve.diff().std() * np.sqrt(DAYS_IN_YEAR),
+        "Sharpe": sharpe(acc_curve),
+    }
+    return pd.Series(stats)
+
+
+def sharpe(acc_curve: pd.DataFrame) -> float:
+    mean = acc_curve.diff().mean()
+    std = acc_curve.diff().std()
+    return np.sqrt(DAYS_IN_YEAR) * mean / std
+
+
+def rolling_sharpe(acc_curve: pd.DataFrame, window: int) -> float:
+    mean = acc_curve.diff().rolling(window).mean()
+    std = acc_curve.diff().rolling(window).std()
+    return np.sqrt(DAYS_IN_YEAR) * mean / std
